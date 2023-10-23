81c96fc2685a45944c6ad22d64c7dfcfcab89fa0
Yousufjoy <kanekiken333@gmail.com>
2023-10-23 19:45:48 +0600

    add product functionality has been added

--- index.js
+++ index.js
@@ -25,13 +25,23 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+
+    const productCollection = client.db("autoConnectDB").collection("product");
+
+    app.post("/products", async (req, res) => {
+      const newData = req.body;
+      console.log(newData);
+      const result = await productCollection.insertOne(newData);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
