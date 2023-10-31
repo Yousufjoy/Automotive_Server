f2f02cf3ec1dd9b3e9293f1e36d9c1065f50d900
Yousufjoy <kanekiken333@gmail.com>
2023-11-01 01:46:46 +0600

    delete and getting cart info etc has been done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -27,12 +27,62 @@ async function run() {
     // Send a ping to confirm a successful connection
 
     const productCollection = client.db("autoConnectDB").collection("product");
+    const cartCollection = client.db("autoConnectDB").collection("cart");
 
-    // 1) Create
+    // 1) Create / Post for products
     app.post("/products", async (req, res) => {
       const newData = req.body;
       console.log(newData);
       const result = await productCollection.insertOne(newData);
+      res.send(result);
+    });
+
+    // Add /Post/ to Cart
+    app.post("/cart", async (req, res) => {
+      const newData = req.body;
+      console.log(newData);
+      const result = await cartCollection.insertOne(newData);
+      res.send(result);
+    });
+
+    // Read mycart
+
+    app.get("/cart", async (req, res) => {
+      const cursor = cartCollection.find(); // cursor mane ekta pointer set kortesi oi collection er moddhe
+      const result = await cursor.toArray(); // joto gula item ase sob diye dibe
+      res.send(result);
+    });
+
+    //Delete from cart
+
+    // 3) Delete
+
+    app.delete("/cart/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // 1.1) Update product
+
+    app.put("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedCar = req.body;
+      const car = {
+        $set: {
+          name: updatedCar.name,
+          image: updatedCar.image,
+          brandName: updatedCar.brandName,
+          type: updatedCar.type,
+          price: updatedCar.price,
+          description: updatedCar.description,
+          rating: updatedCar.rating,
+        },
+      };
+      const result = await productCollection.updateOne(filter, car, options);
       res.send(result);
     });
 
